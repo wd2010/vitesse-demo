a4fc79a8e2b967c7bc01d343aa99a7c762c64aa1
wendan_kzx <wendan_kzx@citicbank.com>
2024-08-13 20:40:39 +0800

    22

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,16 +2,6 @@
 defineOptions({
   name: 'IndexPage',
 })
-const user = useUserStore()
-const name = ref(user.savedName)
-
-const router = useRouter()
-function go() {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
-}
-
-const { t } = useI18n()
 
 const promptEvent = ref(null)
 
@@ -53,35 +43,12 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 <template>
   <div>
-    <div text-4xl>
-      <div i-carbon-campsite inline-block />
-    </div>
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-        Vitesse
-      </a>
-    </p>
-    <p>
-      <em text-sm opacity-75>{{ t('intro.desc') }}</em>
-    </p>
-
-    <div py-4 />
-
-    <TheInput
-      v-model="name"
-      :placeholder="t('intro.whats-your-name')"
-      autocomplete="false"
-      @keydown.enter="go"
-    />
-    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
-
     <div>
       <button
         m-3 text-sm btn
-        :disabled="!name"
         @click="showInstallPromotion"
       >
-        {{ t('button.go') }}
+        点击
       </button>
     </div>
   </div>
